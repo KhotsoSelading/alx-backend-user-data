02045cb743c2fa3aa6fa936c2854a26b626556c8
KhotsoSelading <k.selading007@gmail.com>
2024-02-14 17:47:54 +0200

    All 0x01 Tasks - Khotso

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+"""
+Topic: Basic authentication
+Author: Khotso Selading
+Date: 12-02-2024
+"""
+
+import re
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """
+    Class to manage API authentication.
+
+    Methods:
+    - require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    Returns False.
+    - authorization_header(self, request=None) -> str: Returns None.
+    - current_user(self, request=None) -> TypeVar('User'): Returns None.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Check if authentication is required for the given path.
+
+        Args:
+            path (str): The path to check for authentication requirement.
+            excluded_paths (List[str]): List of paths that are excluded from
+            authentication checks.
+
+        Returns:
+            bool: False, indicating that authentication is not required.
+        """
+        if path is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("*"):
+                regex_pattern = re.escape(excluded_path[:-1]) + ".*"
+                if re.match(regex_pattern, path):
+                    return False
+            elif path.startswith(excluded_path.rstrip('/')):
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Get the authorization header.
+
+        Args:
+            request: The Flask request object.
+
+        Returns:
+            str: None, indicating no authorization header is present.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
+        """
+        Get the current user.
+
+        Args:
+            request: The Flask request object.
+
+        Returns:
+            TypeVar('User'): None, indicating no current user is authenticated.
+        """
+        return None
