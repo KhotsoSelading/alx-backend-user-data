135eb01de7d15240b0c2e80db81b92b358818353
KhotsoSelading <k.selading007@gmail.com>
2024-02-14 18:37:53 +0200

    Task Complete

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,3 +30,30 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the user ID associated with a given session ID.
+
+        Args:
+            session_id (str): The session ID.
+
+        Returns:
+            str: The user ID associated with the session ID.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Retrieves the user associated with the request.
+
+        Args:
+            request (Request, optional): The Flask request object.
+
+        Returns:
+            User: The user associated with the request.
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_by_session_id.get(session_id)
+        return User.get(user_id)
