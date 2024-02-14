62e6a05fe4ddb17f0d9ffd643fa8b7f7364878af
KhotsoSelading <k.selading007@gmail.com>
2024-02-14 18:53:07 +0200

    Task Complete

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,15 +52,17 @@ def forbidden(error) -> str:
 def before_request_error() -> str:
     """Before request handler
     """
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+             '/api/v1/auth_session/login/']
     if (auth is None or auth.require_auth(request.path, paths) is False):
         pass
+    elif (auth.authorization_header(request)
+          is None and auth.session_cookie(request) is None):
+        abort(401)
     elif (auth.authorization_header(request) is None):
         abort(401)
-    else:
-        request.current_user = auth.current_user(request)
-        if (auth.current_user(request) is None):
-            abort(403)
+    elif (auth.current_user(request) is None):
+        abort(403)
 
 
 if __name__ == "__main__":
