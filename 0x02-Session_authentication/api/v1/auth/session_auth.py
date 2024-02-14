bed50698cd57d1d2031a99a663f9a514a14a4ba9
KhotsoSelading <k.selading007@gmail.com>
2024-02-14 18:11:18 +0200

    Task Complete - Khotso

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Topic: Session Authentication
+Author: Khotso Selading
+Date: 14-02-2024
+"""
+
+from api.v1.auth.auth import Auth
+from uuid import uuid4
+from models.user import User
+
+
+class SessionAuth(Auth):
+    pass
