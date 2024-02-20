c88d773927ce66f5dc5d65b672567828b2027cf0
KhotsoSelading <k.selading007@gmail.com>
2024-02-20 07:28:27 +0200

    db.py complete - Khotso

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+Topic: User Authentication Service
+Author: Khotso Selading
+Date: 19-02-2024
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError, IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
+from typing import TypeVar
+
+VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Adds a new user to the Database.
+        """
+        """Add a new user to the database.
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The created User object.
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        try:
+            self._session.add(user)
+            self._session.commit()
+            return user
+        except IntegrityError:
+            self._session.rollback()
+            raise ValueError("User already exists with the given email.")
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user in the database by the specified criteria.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments representing the query
+            criteria.
+
+        Returns:
+            User: The first user found matching the criteria.
+
+        Raises:
+            NoResultFound: If no user is found matching the criteria.
+            InvalidRequestError: If invalid query arguments are passed.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except NoResultFound:
+            raise NoResultFound(
+                "No user found matching the specified criteria."
+                )
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments.")
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user's attributes in the database.
+
+        Args:
+            user_id (int): The ID of the user to update.
+            **kwargs: Arbitrary keyword arguments representing the attributes
+            to update.
+
+        Raises:
+            ValueError: If an argument that does not correspond to a user
+            attribute is passed.
+        """
+        user = self.find_user_by(id=user_id)
+        for attr, value in kwargs.items():
+            if hasattr(User, attr):
+                setattr(user, attr, value)
+            else:
+                raise ValueError(f"Invalid attribute: {attr}")
+        self._session.commit()
